18ca37aa56ba2a036f453ebddf9e28966778f832
joeyaza <JAzadehdel@gmail.com>
2016-01-31 19:55:56 +0000

    background image resizing

--- two/app.js
+++ two/app.js
@@ -2,11 +2,11 @@ $('#datepicker').datepicker({ dateFormat: 'dd-mm-yy' });
 $(document).ready(function(){
    $( "#datepicker1" ).datepicker({
 
-     onClose: function() {
+     onSelect: function() {
        var date2 = $('#datepicker1').datepicker('getDate');
        date2.setDate(date2.getDate()+5)
        $( "#datepicker2" ).datepicker("setDate", date2);
     }
  });
-   $( "#datepicker2" ).datepicker({ dateFormat: 'dd-mm-yy' });
+   $("#datepicker2").datepicker({dateFormat: 'dd-mm-yy'}).datepicker("option", "disabled", true);
 });
