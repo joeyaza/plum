d6eb0f7b2a1298dd99b24dc70cf04728ea157694
joeyaza <JAzadehdel@gmail.com>
2016-01-31 20:55:03 +0000

    all working for background pic resize

--- three/js/controller.js
+++ three/js/controller.js
@@ -4,7 +4,6 @@ angular
 
 MainController.$inject = ['$scope','$http']
 
-
 function MainController($scope, $http) {
   $scope.main = [];
   $scope.info = [];
@@ -12,13 +11,11 @@ function MainController($scope, $http) {
   .success(function(data) {
     $scope.main = data;
     $scope.topten = data.slice(0,10);
-    console.log($scope.topten);
     for (i = 0; i < $scope.topten.length; i++) { 
       $http.get('https://hacker-news.firebaseio.com/v0/item/'+$scope.topten[i]+'.json')
-        .success(function(data) {
-          $scope.info.push(data);
-          console.log($scope.info);
-        })
+      .success(function(data) {
+        $scope.info.push(data);
+      })
     }
   })
 };
